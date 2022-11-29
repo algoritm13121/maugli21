88d3141fbb7f0f67024ac21e328912df0370b6c1
algoritm13121 <algoritm13121@gmail.com>
2022-11-30 00:00:36 +0300

    first commit

---
+++ server/seeds/data/admins/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable max-len */
+const md5 = require('md5');
+
+module.exports = {
+    login: 'admin',
+    password: md5('admin'),
+    email: 'coca22052022@gmail.com',
+    id: 'admin_id'
+};
